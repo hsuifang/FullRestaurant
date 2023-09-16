12e470a129e1615e482110b130d7d21084a753f7
hsuifang <whalekiller03@gmail.com>
2023-09-16 11:04:52 +0800

    feat: Add About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {
-  // AboutUs,
+  AboutUs,
   // Chef,
   // FindUs,
   // Footer,
@@ -19,10 +19,10 @@ function App() {
     <div>
       <Navbar />
       <Header />
+      <AboutUs />
     </div>
   );
 }
-// <AboutUs />
 // <SpecialMenu />
 // <Chef />
 // <Intro />
